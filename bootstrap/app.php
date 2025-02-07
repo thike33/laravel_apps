c023059eeb0132478fd928c69d1c311b16b83e26
thike33 <teke3business@gmail.com>
2025-02-07 23:03:32 +0900

    ミドルウェア実装

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware
+            ->redirectGuestsTo('/login')
+            ->redirectUsersTo('/');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,19 @@ use Illuminate\Support\Facades\Route;
 // トップページ
 Route::view('/', 'index')->name('top');
 
-// ユーザー
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
+// 非ログイン時
+Route::middleware('guest')->group(function () {
+    // ユーザー登録
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
-// ログイン
-Route::get('/login', [AuthController::class, 'LoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+    // ログイン
+    Route::get('/login', [AuthController::class, 'LoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
-// ログアウト
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// ログイン時
+Route::middleware('auth')->group(function () {
+    // ログアウト
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
